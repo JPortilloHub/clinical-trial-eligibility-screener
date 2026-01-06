837378c67fc41d6a9513be4542ff6bef53770217
JPortilloHub <58309995+JPortilloHub@users.noreply.github.com>
2026-01-06 17:07:08 +0000

    pdf extraction

--- elgibility-screener.py
+++ elgibility-screener.py
@@ -1,13 +1,20 @@
 #!git clone https://github.com/JPortilloHub/clinical-trial-eligibility-screener.git
-#import anthropic
+import anthropic
+
 from pypdf import PdfReader
+import os
+
+client = anthropic.Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
 
 #1. Read clinical protocol and save it in a string
 file_path = "clinical-trial-protocol.pdf"
-clinical_trial_protocol = PdfReader(file_path)
+reader = PdfReader(file_path)
+clinical_trial_protocol = ""
+for page in reader.pages:
+    clinical_trial_protocol += page.extract_text()
 
 #2. and extract the part you are interested in: Eligibility criteria
-extraction_prompt: f"""" You are a a clinician, and your task is to extract the section Patient Selection Criteria, 
+extraction_prompt= f"""" You are a a clinician, and your task is to extract the section Patient Selection Criteria, 
 including Inclusion Criteria and Exclusion Criteria from the document in pdf wihtin curly brackets. 
 Return only the text of that section
 
@@ -15,3 +22,13 @@ Return only the text of that section
 {clinical_trial_protocol}
 </document>
 """
+
+
+response = client.messages.create(
+    model = "claude-haiku-4-5-20251001",
+    max_tokens = 2000,
+    messages = [{"role": "user", "content": extraction_prompt}]
+)
+
+eligibility_criteria_extraction = response.content[0].text
+
